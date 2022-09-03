2eab2af7d755347d6d467d679bb9f5df7bf37bef
irenee-14 <74870834+irenee-14@users.noreply.github.com>
2022-09-03 15:11:54 +0900

    1157 - 단어공부

---
+++ class1/n1157.py
@@ -0,0 +1,15 @@
+'''
+1157 - 단어 공부
+2022.9.3
+'''
+
+word = input().upper()
+word_list = list(set(word))
+dic = []
+
+for i in word_list:
+    dic.append(word.count(i))
+if dic.count(max(dic)) > 1:
+    print("?")
+else:
+    print(word_list[dic.index(max(dic))])
