398d5d86832f0efeeed50028a7713e61729b6eb8
irenee-14 <74870834+irenee-14@users.noreply.github.com>
2022-09-11 23:20:47 +0900

    9498 - 시험성적

---
+++ class1/n9498.py
@@ -0,0 +1,16 @@
+'''
+2022.9.11
+9498 - 시험성적
+'''
+
+test = int(input())
+if (test >= 90) and (test <= 100):
+    print('A')
+elif (test >= 80) and (test <= 89):
+    print('B')
+elif (test >= 70) and (test <= 79):
+    print('C')
+elif (test >= 60) and (test <= 69):
+    print('D')
+else:
+    print('F')
