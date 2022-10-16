b4753ff323ac144b7809302e5b6a6843b5bf7926
irenee-14 <74870834+irenee-14@users.noreply.github.com>
2022-10-16 18:42:08 +0900

    4949 - 균형잡힌 세상

---
+++ class2/n4949.py
@@ -0,0 +1,31 @@
+'''
+2022.10.16
+4949 - 균형잡힌 세상
+'''
+
+while True:
+    line = input()
+    stack = []
+
+    if line == '.':
+        break
+
+    for i in line:
+        if i == '[' or i == '(':
+            stack.append(i)
+        elif i == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(']')
+                break
+        elif i == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(')')
+                break
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
