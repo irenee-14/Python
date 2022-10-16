27dafd8213025c571a5c03e59e36f1d32573fffd
irenee-14 <74870834+irenee-14@users.noreply.github.com>
2022-10-16 18:41:58 +0900

    2869 - 달팽이는 올라가고 싶다

--- class2/n2869.py
+++ class2/n2869.py
@@ -7,7 +7,7 @@ import sys
 input = sys.stdin.readline
 
 A, B, V = map(int, input().split())
-if (V-B) % (A-B) == 0 :
+if (V-B) % (A-B) == 0:
     print((V-B) // (A-B))
 else:
     print(((V-B) // (A-B)) + 1)
