7b8c160e984eb3b25464a30c42901cbdf28e3d5b
irenee-14 <74870834+irenee-14@users.noreply.github.com>
2022-10-27 07:02:15 +0900

    1264 - 모음의 개수

---
+++ bronze/n1264.py
@@ -0,0 +1,13 @@
+'''
+2022.10.27
+1264 - 모음의 개수
+'''
+while True:
+    s = input()
+    if s == '#':
+        break
+    cnt = 0
+    for c in s:
+        if c in 'aeiouAEIOU':
+            cnt += 1
+    print(cnt)
