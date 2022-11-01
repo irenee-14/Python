2aa133ac84f8c268220682d015d588525d190180
irenee-14 <74870834+irenee-14@users.noreply.github.com>
2022-11-01 21:37:07 +0900

    2480 - 주사위 세개

---
+++ bronze/n2480.py
@@ -0,0 +1,17 @@
+'''
+2022.11.1
+2480 - 주사위 세개
+'''
+
+one, two, three = map(int, input().split())
+
+if one == two == three:
+    print(10000 + one * 1000)
+elif one == two:
+    print(1000 + one * 100)
+elif one == three:
+    print(1000 + one * 100)
+elif two == three:
+    print(1000 + two * 100)
+else:
+    print(100 * max(one, two, three))
