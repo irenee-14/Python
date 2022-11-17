b0723ff37d6e725613deac4d8d22704dbb71e296
irenee-14 <74870834+irenee-14@users.noreply.github.com>
2022-11-17 13:39:34 +0900

    1927 - 최소 힙

---
+++ class3/n1927.py
@@ -0,0 +1,21 @@
+'''
+2022.11.17
+1927 - 최소 힙
+'''
+
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if not heap:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, x)
