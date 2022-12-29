f9de3583d2b75e44ce23ec76f9c1cfeacda2b56d
irenee-14 <74870834+irenee-14@users.noreply.github.com>
2022-12-29 11:24:03 +0900

    11279 - 최대 힙

---
+++ class3/n11279.py
@@ -0,0 +1,21 @@
+'''
+2022.12.29
+11279 - 최대 힙
+'''
+
+import sys
+import heapq as hq
+
+input = sys.stdin.readline
+n = int(input())
+heap = []
+
+for i in range(n):
+    x = int(input())
+    if x:
+        hq.heappush(heap, (-x, x))
+    else:
+        if len(heap) >= 1:
+            print(hq.heappop(heap)[1])
+        else:
+            print(0)
