129c1cc1e94f7a325708e1c22695ebec2ee6d6be
irenee-14 <74870834+irenee-14@users.noreply.github.com>
2023-03-10 14:07:14 +0900

    16953 - A → B

---
+++ silver/n16953.py
@@ -0,0 +1,20 @@
+'''
+2023.3.10
+16953 - A → B
+'''
+a, b = map(int, input().split())
+cnt = 1
+
+while b != a:
+    cnt += 1
+    temp = b
+    if b % 10 == 1:
+        b //= 10
+    elif b % 2 == 0:
+        b //= 2
+    
+    if temp == b:
+        print(-1)
+        break
+else:
+    print(cnt)
