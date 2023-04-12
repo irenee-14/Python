b907394c26efab8e9854e9fc88b7cecae68a0f83
irenee-14 <74870834+irenee-14@users.noreply.github.com>
2023-04-12 14:19:12 +0900

    1476 - 날짜 계산

---
+++ silver/n1476.py
@@ -0,0 +1,13 @@
+'''
+2023.4.12
+1476 - 날짜 계산
+'''
+e, s, m = map(int, input().split())
+year = 1
+
+while True:
+    if ((year - e) % 15 == 0) and ((year - s) % 28 == 0) and ((year - m) % 19 == 0):
+        break 
+    year += 1
+
+print(year)
