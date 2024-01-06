2f8852a99e7f9213d99270a0a0ce7d45cfd036fe
irenee-14 <ireneee145@gmail.com>
2024-01-07 00:52:33 +0900

    4101 - 크냐?

---
+++ bronze/n4101.py
@@ -0,0 +1,12 @@
+'''
+2024.1.6
+4101 - 크냐?
+'''
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    if a > b:
+        print("Yes")
+    else:
+        print("No")
