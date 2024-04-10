827af2dfd6b842ee801c678e7946b04dd310c884
irenee-14 <ireneee145@gmail.com>
2024-04-10 22:39:57 +0900

    1547 - 공

---
+++ bronze/n1547.py
@@ -0,0 +1,16 @@
+'''
+2024.4.10
+1547 - 공
+'''
+N = int(input())
+
+cups = [1, 2, 3]
+for _ in range(N):
+    x, y = map(int, input().split())
+
+    xi = cups.index(x)
+    yi = cups.index(y)
+
+    cups[xi], cups[yi] = cups[yi], cups[xi]
+
+print(cups[0])
