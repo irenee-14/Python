55f86b40eea53b81f2a27f6c3dc878056fcf61f5
Jihyun Lim <74870834+irenee-14@users.noreply.github.com>
2024-04-13 14:38:39 +0900

    1026 - 보물

---
+++ silver/n1026.py
@@ -0,0 +1,14 @@
+'''
+2024.4.11
+1026 - 보물
+'''
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+res = 0
+for i in range(N):
+    res += min(A) * max(B)
+    A.pop(A.index(min(A)))
+    B.pop(B.index(max(B)))
+print(res)
