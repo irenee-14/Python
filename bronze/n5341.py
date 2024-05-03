282756500dfd9a18906e615ca4fcc118705ee890
irenee-14 <ireneee145@gmail.com>
2024-05-04 00:35:47 +0900

    5341 - Pyramids

---
+++ bronze/n5341.py
@@ -0,0 +1,14 @@
+'''
+2024.5.4
+5341 - Pyramids
+'''
+
+while True:
+    sum = 0
+    n = int(input())
+    if not n:
+        break
+
+    for i in range(1, n + 1):
+        sum += i
+    print(sum)
