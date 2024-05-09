10aca5847944fa3cb1922ce8fa3d31982b3a776b
Jihyun Lim <74870834+irenee-14@users.noreply.github.com>
2024-05-09 21:39:57 +0900

    5928 - Contest Timing

---
+++ bronze/n5928.py
@@ -0,0 +1,13 @@
+'''
+2024.5.9
+5928 - Contest Timing
+'''
+D, H, M = map(int, input().split())
+t1 = D * 24 * 60 + H * 60 + M
+t2 = 11 * 24 * 60 + 11 * 60 + 11
+t = t1 - t2
+
+if t < 0:
+    print(-1)
+else:
+    print(t)
