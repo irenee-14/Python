ac496306072d63de89e9ced8b4c975a6fd4b0ad3
irenee-14 <ireneee145@gmail.com>
2024-06-02 22:43:03 +0900

    2765 - 자전거 속도

---
+++ bronze/n2765.py
@@ -0,0 +1,16 @@
+'''
+2024.6.2
+2765 - 자전거 속도
+'''
+
+from math import pi
+
+i = 1
+while 1:
+    d, r, t = map(float, input().split())
+    if r == 0:
+        break
+    dis = d/63360 * pi * r
+    mph = dis / t * 3600
+    print("Trip #%d: %.2f %.2f" % (i, dis, mph))
+    i += 1
