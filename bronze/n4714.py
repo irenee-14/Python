197197e906d097d19dfb8312df25da89b21c0e71
irenee-14 <ireneee145@gmail.com>
2024-06-03 23:29:50 +0900

    4714 - Lunacy

---
+++ bronze/n4714.py
@@ -0,0 +1,12 @@
+'''
+2024.6.3
+4714 - Lunacy
+'''
+
+while True:
+    weight = float(input())
+
+    if weight == -1.0:
+        break
+
+    print("Objects weighing %.2f on Earth will weigh %.2f on the moon." % (weight, weight * 0.167))
