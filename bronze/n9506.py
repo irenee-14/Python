4326c5183e1430fa5d5471d344c4214ca1dd9305
irenee-14 <ireneee145@gmail.com>
2024-06-14 21:58:19 +0900

    9506 - 약수들의 합

---
+++ bronze/n9506.py
@@ -0,0 +1,17 @@
+'''
+2024.6.14
+9506 - 약수들의 합
+'''
+
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    arr = []
+    for i in range(1, n):
+        if n % i == 0:
+            arr.append(i)
+    if sum(arr) == n:
+        print(n, " = ", " + ".join(str(i) for i in arr), sep="")
+    else:
+        print(n, "is NOT perfect.")
