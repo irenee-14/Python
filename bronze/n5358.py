9a6419fb8f2c276f2ff9570e201f7c9a6c00b67f
irenee-14 <ireneee145@gmail.com>
2024-06-15 23:32:07 +0900

    5358 - Football Team

---
+++ bronze/n5358.py
@@ -0,0 +1,22 @@
+'''
+2024.6.15
+5358 - Football Team
+'''
+
+try:
+    while True :
+        s = input()
+        for i in range(len(s)):
+            if s[i] == 'e':
+                print("i", end="")
+            elif s[i] == 'i':
+                print("e", end="")
+            elif s[i] == 'E':
+                print("I", end="")
+            elif s[i] == 'I':
+                print("E", end="")
+            else:
+                print(s[i], end="")
+        print()
+except:
+    exit(0)
