18f571c5a1a512e990a48f0e932b124ee448d838
irenee-14 <ireneee145@gmail.com>
2024-09-24 23:00:37 +0900

    1343 - 폴리오미노

---
+++ silver/n1343.py
@@ -0,0 +1,13 @@
+'''
+2024.9.24
+1343 - 폴리오미노
+'''
+
+board = input()
+board = board.replace("XXXX", "AAAA")
+board = board.replace("XX", "BB")
+
+if 'X' in board:
+    print(-1)
+else:
+    print(board)
