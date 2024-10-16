bd83a0d82642c0315285d12700577dd07b71ac59
irenee-14 <ireneee145@gmail.com>
2024-10-16 23:36:13 +0900

    1063 - 킹

---
+++ silver/n1063.py
@@ -0,0 +1,24 @@
+'''
+2024.10.16
+1063 - 킹
+'''
+
+move = {'R':(0,1), 'L':(0,-1), 'B':(1,0), 'T':(-1,0), 'RT':(-1,1), 'LT':(-1,-1), 'RB':(1,1), 'LB':(1,-1)}
+chess = ['A', 'B' ,'C', 'D', 'E', 'F', 'G', 'H']
+
+k, s, n = input().split()
+
+k1, k2 = (8 - (int(k[1])), chess.index(k[0])) # 킹의 열, 행
+s1, s2 = (8 - (int(s[1])), chess.index(s[0])) # 돌의 열, 행
+
+for i in range(int(n)):
+    a, b = move[input()]
+    if 0 <= k1 + a < 8 and 0 <= k2 + b < 8:
+        k1, k2 = k1 + a, k2 + b
+        if k1 == s1 and k2 == s2:
+            s1, s2 = s1 + a, s2 + b
+        if s1 < 0 or s1 >= 8 or s2 < 0 or s2 >= 8:
+            k1, k2 = k1 - a, k2 - b
+            s1, s2 = s1 - a, s2 - b
+print(chess[k2], 8 - k1, sep='')
+print(chess[s2], 8 - s1, sep='')
