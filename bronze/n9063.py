02ed96dfcc3624ac1c5f7d60e771848ef4ab9e8e
irenee-14 <ireneee145@gmail.com>
2024-12-23 23:15:19 +0900

    9063 - 대지

---
+++ bronze/n9063.py
@@ -0,0 +1,12 @@
+'''
+2024.12.23
+9063 - 대지
+'''
+
+n = int(input())
+X, Y = [], []
+for _ in range(n):
+    x, y = map(int, input().split())
+    X.append(x)
+    Y.append(y)
+print((max(X) - min(X)) * (max(Y) - min(Y)))
