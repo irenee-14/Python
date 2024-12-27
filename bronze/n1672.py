660243ef51fca293a8a24b819aa0a48f04a16988
irenee-14 <ireneee145@gmail.com>
2024-12-27 23:19:55 +0900

    1672 - DNA 해독

---
+++ bronze/n1672.py
@@ -0,0 +1,22 @@
+'''
+2024.12.27
+1672 - DNA 해독
+'''
+
+N = int(input())
+s = list(input())
+
+dna = {"AA" : "A", "AG" : "C", "AC" : "A", "AT" : "G", "GA" : "C","GG" : "G",
+       "GC" : "T", "GT" : "A", "CA" : "A", "CG" : "T", "CC" : "C", "CT" : "G",
+       "TA" : "G", "TG" : "A", "TC" : "G","TT" : "T"}
+
+while True:
+    if len(s) == 1:
+        break
+    ss = s[-2] + s[-1]
+    if ss in dna:
+        del s[-2:]
+        s.append(dna.get(ss))
+print(*s)
+
+
