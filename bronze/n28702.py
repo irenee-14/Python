1fd5acb3f6decf72987f4ed7d3c710ead4901005
irenee-14 <ireneee145@gmail.com>
2025-01-10 22:50:44 +0900

    28702 - FizzBuzz

---
+++ bronze/n28702.py
@@ -0,0 +1,17 @@
+'''
+2024.1.10
+28702 - FizzBuzz
+'''
+
+for i in range(3, 0, -1):
+    x = input()
+    if x not in ["Fizz", "Buzz", "FizzBuzz"]:
+        n = int(x) + i
+if not n % 3 and not n % 5:
+    print("FizzBuzz")
+elif not n % 3 and n % 5:
+    print("Fizz")
+elif n % 3 and not n % 5:
+    print("Buzz")
+else:
+    print(n)
