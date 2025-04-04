7a4f89e14642f4e9f24c3ea32911e8c40ae818ab
irenee-14 <ireneee145@gmail.com>
2025-04-04 21:57:03 +0900

    5576 - 콘테스트

---
+++ bronze/n5576.py
@@ -0,0 +1,16 @@
+'''
+2025.4.4
+5576 - 콘테스트
+'''
+
+W = []
+K = []
+
+for i in range(20):
+    if i < 10:
+        W.append(int(input()))
+    else:
+        K.append(int(input()))
+K.sort()
+W.sort()
+print((W[-1] + W[-2] + W[-3]), (K[-1] + K[-2] + K[-3]))
