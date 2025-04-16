c08779b4de5beaa012d57d5b0e9694cdf066eb62
irenee-14 <ireneee145@gmail.com>
2025-04-16 19:23:15 +0900

    23804 - 골뱅이 찍기 - ㄷ

---
+++ bronze/n23804.py
@@ -0,0 +1,15 @@
+'''
+2025.4.16
+23804 - 골뱅이 찍기 - ㄷ
+'''
+
+n = int(input())
+
+for _ in range(n):
+    print("@@@@@"*n)
+
+for _ in range(n*3):
+    print("@"*n)
+
+for _ in range(n):
+    print("@@@@@"*n)
