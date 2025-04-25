8318c812ac779ee2fc9c916a524c5cb9976b4dd4
irenee-14 <ireneee145@gmail.com>
2025-04-25 16:12:08 +0900

    9372 - 상근이의 여행

---
+++ silver/n9372.py
@@ -0,0 +1,12 @@
+'''
+2025.4.25
+9372 - 상근이의 여행
+'''
+
+T = int(input())
+for _ in range(T):
+    N, M = map(int, input().split())
+
+    for _ in range(M):
+        a, b = map(int, input().split())
+    print(N - 1)
