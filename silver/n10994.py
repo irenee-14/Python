77d700b989fc98aa637953e408eab1ebdf22b957
irenee-14 <ireneee145@gmail.com>
2025-05-12 23:04:39 +0900

    10994 - 별 찍기 - 19

---
+++ silver/n10994.py
@@ -0,0 +1,23 @@
+'''
+2025.5.12
+10994 - 별 찍기 - 19
+'''
+
+n = int(input())
+m = 4*n-3
+a = [[' ']*m for _ in range(m)]
+
+
+def r(i):
+    if i == m + 1:
+        return
+    for j in range(i, m - i):
+        a[i][j], a[m-i-1][j] = '*', '*'
+    for j in range(i, m - i):
+        a[j][i], a[j][m-i-1] = '*', '*'
+    r(i + 2)
+
+
+r(0)
+for i in a:
+    print(*i, sep='')
