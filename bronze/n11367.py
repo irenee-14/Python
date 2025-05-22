7f9afa7792cc5a86670a625267b9e6cda4906e9a
irenee-14 <ireneee145@gmail.com>
2025-05-22 22:14:34 +0900

    11367 - Report Card Game

---
+++ bronze/n11367.py
@@ -0,0 +1,27 @@
+'''
+2025.5.22
+11367 - Report Card Game
+'''
+
+for _ in range(int(input())):
+    s, n = input().split()
+    n = int(n)
+    if n >= 97:
+        res = "A+"
+    elif n >= 90:
+        res = "A"
+    elif n >= 87:
+        res = "B+"
+    elif n >= 80:
+        res = "B"
+    elif n >= 77:
+        res = "C+"
+    elif n >= 70:
+        res = "C"
+    elif n >= 67:
+        res = "D+"
+    elif n >= 60:
+        res = "D"
+    else:
+        res = "F"
+    print(s, res)
