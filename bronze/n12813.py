46fd1f37438868d74350aa13a9259181404b8c30
irenee-14 <ireneee145@gmail.com>
2025-06-03 20:44:31 +0900

    12813 - 이진수 연산

---
+++ bronze/n12813.py
@@ -0,0 +1,17 @@
+'''
+2025.6.3
+12813 - 이진수 연산
+'''
+
+a = int(input(), 2)
+b = int(input(), 2)
+
+n = 100000
+mask = 2 ** n - 1
+
+print(bin(a & b)[2:].zfill(n))
+print(bin(a | b)[2:].zfill(n))
+print(bin(a ^ b)[2:].zfill(n))
+print(bin(a ^ mask)[2:].zfill(n))
+print(bin(b ^ mask)[2:].zfill(n))
+
