2c6ab67912c6b4643812991ee2700f63a0c763af
irenee-14 <ireneee145@gmail.com>
2025-06-05 22:29:44 +0900

    5598 - 카이사르 암호

---
+++ bronze/n5598.py
@@ -0,0 +1,12 @@
+'''
+2025.6.5
+5598 - 카이사르 암호
+'''
+
+li = list(input())
+for i in range(len(li)):
+    k = ord(li[i]) - 3
+    if k < ord('A'):
+        k += 26
+    li[i] = chr(k)
+print(''.join(li))
