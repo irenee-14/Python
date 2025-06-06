6644b9aefa6392c4b4a13a8d1422b2999f77b285
irenee-14 <ireneee145@gmail.com>
2025-06-06 22:26:40 +0900

    23812 - 골뱅이 찍기 - 돌아간 ㅍ

---
+++ bronze/n23812.py
@@ -0,0 +1,21 @@
+'''
+2025.6.6
+23812 - 골뱅이 찍기 - 돌아간 ㅍ
+'''
+
+n = int(input())
+
+for _ in range(n):
+    print('@'*n + ' '*(3*n) + '@'*n)
+
+for _ in range(n):
+    print('@'*(5*n))
+
+for _ in range(n):
+    print('@'*n + ' '*(3*n) + '@'*n)
+
+for _ in range(n):
+    print('@' * (5 * n))
+
+for _ in range(n):
+    print('@' * n + ' ' * (3 * n) + '@' * n)
