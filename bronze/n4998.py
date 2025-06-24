fa93c2d29a1b6ac7e8fa966bc0069b550f887a98
irenee-14 <ireneee145@gmail.com>
2025-06-25 00:13:35 +0900

    4998 - 저금

---
+++ bronze/n4998.py
@@ -0,0 +1,16 @@
+'''
+2025.6.25
+4998 - 저금
+'''
+
+try:
+    while (1):
+        n, b, m = map(float, input().split())
+        cnt = 0
+        while n < m:
+            n += n * (b / 100)
+            cnt += 1
+        print(cnt)
+
+except:
+    exit()
