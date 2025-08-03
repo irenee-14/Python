0b9633148a97524c571eb6a45c897a51811ab981
irenee-14 <ireneee145@gmail.com>
2025-08-03 21:53:20 +0900

    3499 - 해밍 거리

---
+++ bronze/n3449.py
@@ -0,0 +1,13 @@
+'''
+2025.8.3
+3499 - 해밍 거리
+'''
+
+for _ in range(int(input())):
+    a = input();
+    b = input()
+    cnt = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            cnt += 1
+    print(f"Hamming distance is {cnt}.")
