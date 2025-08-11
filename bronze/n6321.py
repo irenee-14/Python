828fdd6d9f4775a3ec8df64d697c434c5bc850a5
irenee-14 <ireneee145@gmail.com>
2025-08-11 22:31:14 +0900

    6321 - IBM 빼기 1

---
+++ bronze/n6321.py
@@ -0,0 +1,19 @@
+'''
+2025.8.11
+6321 - IBM 빼기 1
+'''
+
+n = int(input())
+
+for x in range(1, n + 1):
+    data = input()
+    result = ''
+    for i in range(len(data)):
+        value = ord(data[i]) + 1
+        if value > 90:
+            value = 65
+        result += chr(value)
+
+    print('String #%d' % x)
+    print(result)
+    print()
