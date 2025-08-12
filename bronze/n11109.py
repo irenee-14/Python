7b6b7f23065b541cefa183f4a8117c7d4efe3f17
irenee-14 <ireneee145@gmail.com>
2025-08-12 23:21:57 +0900

    11109 - 괴짜 교수

---
+++ bronze/n11109.py
@@ -0,0 +1,13 @@
+'''
+2025.8.12
+11109 - 괴짜 교수
+'''
+
+for _ in range(int(input())):
+    d, n, s, p = map(int, input().split())
+    if d + p*n == n*s:
+        print("does not matter")
+    elif d + p*n > n*s:
+        print("do not parallelize")
+    else:
+        print("parallelize")
