408403766fa428ee01412f9ecefa31d949e623f5
irenee-14 <ireneee145@gmail.com>
2025-08-19 22:15:43 +0900

    25641 - 균형 잡힌 소떡소떡

---
+++ bronze/n25641.py
@@ -0,0 +1,15 @@
+'''
+2025.8.19
+25641 - 균형 잡힌 소떡소떡
+'''
+
+N = int(input())
+lst = list(reversed(input()))
+
+while 1:
+    if lst.count('s') == lst.count('t'):
+        break
+    else:
+        lst.pop(-1)
+print(''.join(lst.reverse()))
+
