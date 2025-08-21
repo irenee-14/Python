9a3629f22e3d86530cf1e967e91405f78724cec9
irenee-14 <ireneee145@gmail.com>
2025-08-21 22:27:52 +0900

    30957 - 빅데이터 vs 정보보호 vs 인공지능

---
+++ bronze/n30957.py
@@ -0,0 +1,25 @@
+'''
+2025.8.21
+30957 - 빅데이터 vs 정보보호 vs 인공지능
+'''
+
+N = int(input())
+B, S, A = 0, 0, 0
+
+for i in input():
+    if i == "B":
+        B += 1
+    elif i == "S":
+        S += 1
+    elif i == "A":
+        A += 1
+
+if B == S and S == A and B == A:
+    print("SCU")
+else:
+    if B == max(B, S, A):
+        print("B", end="")
+    if S == max(B, S, A):
+        print("S", end="")
+    if A == max(B, S, A):
+        print("A", end="")
