793450fa3a76e136dc890bff520a0e4364564f9c
irenee-14 <ireneee145@gmail.com>
2025-09-06 23:01:05 +0900

    11586 - 지영 공주님의 마법 거울

---
+++ bronze/n11586.py
@@ -0,0 +1,17 @@
+'''
+2025.9.6
+11586 - 지영 공주님의 마법 거울
+'''
+
+n = int(input())
+li = []
+for i in range(n):
+    li.append(input())
+K = int(input())
+if K == 2:
+    for i in range(n):
+        li[i] = li[i][::-1]
+elif K == 3:
+    li = li[::-1]
+for i in range(n):
+    print(li[i])
