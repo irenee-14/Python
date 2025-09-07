5f3038b042aecf75090162056987f85060d93137
irenee-14 <ireneee145@gmail.com>
2025-09-07 15:36:11 +0900

    7510 - 고급 수학

---
+++ bronze/n7510.py
@@ -0,0 +1,13 @@
+'''
+2025.9.7
+7510 - 고급 수학
+'''
+
+for i in range(int(input())):
+    li = sorted(map(int, input().split()))
+    if li[0]**2 + li[1]**2 == li[2]**2:
+        print(f"Scenario #{i+1}:")
+        print("yes\n")
+    else:
+        print(f"Scenario #{i+1}:")
+        print("no\n")
