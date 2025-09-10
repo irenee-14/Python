c360ff71bf1e9cea070d3cd5550d72b71cf8cae6
irenee-14 <ireneee145@gmail.com>
2025-09-10 23:08:36 +0900

    11005 - 진법 변환 2

---
+++ bronze/n11005.py
@@ -0,0 +1,14 @@
+'''
+2025.9.10
+11005 - 진법 변환 2
+'''
+
+N, B = map(int, input().split())
+s = ''
+arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+while N:
+    s += str(arr[N%B])
+    N //= B
+
+print(s[::-1])
