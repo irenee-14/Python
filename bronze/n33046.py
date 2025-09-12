8ee17f8de85128871a4a4636b70894eec179b6f9
irenee-14 <ireneee145@gmail.com>
2025-09-12 19:26:48 +0900

    33046 - Alea Iacta Est

--- bronze/n33046.py
+++ bronze/n33046.py
@@ -3,7 +3,8 @@
 33046 - Alea Iacta Est
 '''
 
-A, B, C, D = map(int, input().split())
+A, B = map(int, input().split())
+C, D = map(int, input().split())
 answer = (A + B - 1) % 4
 answer = (answer + C + D - 1) % 4
 print(answer + 1)
