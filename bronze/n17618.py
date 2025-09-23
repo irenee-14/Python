7a7293870147b4390e4fff4063652161f98bc9ec
irenee-14 <ireneee145@gmail.com>
2025-09-23 22:26:23 +0900

    17618 - 신기한 수

---
+++ bronze/n17618.py
@@ -0,0 +1,15 @@
+'''
+2025.9.23
+17618 - 신기한 수
+'''
+
+n = int(input())
+res = 0
+for number in range(1, n+1):
+    tmp = str(number)
+    digit_sum = 0
+    for i in tmp:
+        digit_sum += int(i)
+    if number % digit_sum == 0:
+        res += 1
+print(res)
