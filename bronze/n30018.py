b64e82aa202b265a3f3c1d72d540c24a7b2e34f0
irenee-14 <ireneee145@gmail.com>
2025-09-24 20:27:40 +0900

    30018 - 타슈

---
+++ bronze/n30018.py
@@ -0,0 +1,14 @@
+'''
+2025.9.24
+30018 - 타슈
+'''
+
+N = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+sum = 0
+for i in range(0, N) :
+    sum += abs(a[i] - b[i])
+
+print(sum // 2)
