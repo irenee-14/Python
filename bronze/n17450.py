1af15eeb1c0250c68f2b259802c1b9968edc6079
irenee-14 <ireneee145@gmail.com>
2025-09-26 22:52:02 +0900

    17450 - 과자 사기

---
+++ bronze/n17450.py
@@ -0,0 +1,23 @@
+'''
+2025.9.26
+17450 - 과자 사기
+'''
+
+import sys
+input = sys.stdin.readline
+
+nums = []
+for i in range(3):
+    a, b = map(int, input().split())
+    if a * 10 >= 5000:
+        a = a * 10 - 500
+    else:
+        a *= 10
+    b *= 10
+    nums.append(b / a)
+if max(nums) == nums[0]:
+    print('S')
+elif max(nums) == nums[1]:
+    print('N')
+else:
+    print('U')
