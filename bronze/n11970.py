cc514df611f474d3e8cb3beff425fed6b684d971
irenee-14 <ireneee145@gmail.com>
2025-10-10 22:47:10 +0900

    11970 - Fence Painting

---
+++ bronze/n11970.py
@@ -0,0 +1,12 @@
+'''
+2025.10.10
+11970 - Fence Painting
+'''
+
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+
+if c >= b or d <= a:
+    print(b-a + d-c)
+else:
+    print( max(b,d) - min(a,c) )
