02cb554ffc0855f62e18b241187036df2a96bc49
irenee-14 <ireneee145@gmail.com>
2025-10-15 22:15:54 +0900

    11874 - ZAMKA

---
+++ bronze/n11874.py
@@ -0,0 +1,15 @@
+'''
+2025.10.15
+11874 - ZAMKA
+'''
+
+L, D = int(input()), int(input())
+X = int(input())
+for i in range(L, D+1):
+    if sum(map(int, list(str(i)))) == X:
+        print(i)
+        break
+for i in range(D, L-1, -1):
+    if sum(map(int, list(str(i)))) == X:
+        print(i)
+        break
