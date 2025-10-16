d4d9f1c539a9395d91106d038c6a4d579b242175
irenee-14 <ireneee145@gmail.com>
2025-10-16 19:01:53 +0900

    19572 - 가뭄(small)

---
+++ bronze/n19572.py
@@ -0,0 +1,14 @@
+'''
+2025.10.16
+19572 - 가뭄(small)
+'''
+
+d1, d2, d3 = map(int, input().split())
+a = (d1+d2-d3)/2
+b = d1-a
+c = d2-a
+if a > 0 and b > 0 and c > 0:
+    print(1)
+    print("%.1f %.1f %.1f" %(a, b, c))
+else:
+    print(-1)
