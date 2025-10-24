10400f1b72b49f6651cd79c2933ed1febe4a8cdf
irenee-14 <ireneee145@gmail.com>
2025-10-24 22:29:41 +0900

    27736 - 찬반투표

---
+++ bronze/n27736.py
@@ -0,0 +1,23 @@
+'''
+2025.10.24
+27736 - 찬반투표
+'''
+
+N = int(input())
+N_list = list(map(int, input().split()))
+approved, rejected, invalid = 0, 0, 0
+
+for i in N_list:
+    if i == 1:
+        approved += 1
+    elif i == -1:
+        rejected += 1
+    else:
+        invalid += 1
+
+if invalid >= N / 2:
+    print('INVALID')
+elif approved > rejected:
+    print('APPROVED')
+else:
+    print('REJECTED')
