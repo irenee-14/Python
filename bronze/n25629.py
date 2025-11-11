7f43b472b702d8ab2fc15b9657584f127bdb3efb
irenee-14 <ireneee145@gmail.com>
2025-11-11 21:05:22 +0900

    25629 - 홀짝 수열

---
+++ bronze/n25629.py
@@ -0,0 +1,21 @@
+'''
+2025.11.11
+25629 - 홀짝 수열
+'''
+
+import math
+
+
+N = int(input())
+List = list(map(int,input().split()))
+a = 0
+b = 0
+for i in range(len(List)):
+    if List[i] % 2 != 0:
+        a+=1
+    else:
+        b+=1
+if a == math.ceil(N/2) and b == N//2:
+    print(1)
+else:
+    print(0)
