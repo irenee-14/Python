c5e827e135c1a7229b45da11137f445a1b441f5b
irenee-14 <ireneee145@gmail.com>
2025-11-13 21:48:15 +0900

    25965 - 미션 도네이션

---
+++ bronze/n25965.py
@@ -0,0 +1,25 @@
+'''
+2025.11.13
+25965 - 미션 도네이션
+'''
+
+
+N = int(input())
+for _ in range(N):
+    M = int(input())
+    cnt = 0
+    lst = []
+    for _ in range(M):
+        lst.append(list(map(int, input().split())))
+    KDA = list(map(int, input().split()))
+
+    for i in lst:
+        tmp = 0
+        for j in range(3):
+            if j != 1:
+                tmp += i[j] * KDA[j]
+            else:
+                tmp -= i[j] * KDA[j]
+        if tmp > 0:
+            cnt += tmp
+    print(cnt)
