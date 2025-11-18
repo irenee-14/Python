b799c913a2553705df2cdf22ccc4bde6b2b644bb
irenee-14 <ireneee145@gmail.com>
2025-11-18 21:40:07 +0900

    16785 - ソーシャルゲーム (Social Game)

---
+++ bronze/n16785.py
@@ -0,0 +1,13 @@
+'''
+2025.11.18
+16785 - ソーシャルゲーム (Social Game)
+'''
+
+A, B, C = map(int, input().split())
+cnt = total = 0
+while total < C:
+    total += A
+    cnt += 1
+    if cnt%7 == 0:
+        total += B
+print(cnt)
