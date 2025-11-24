a5267ef32efd086f74e101ce08b8ad067083d96f
Jihyun Lim <74870834+irenee-14@users.noreply.github.com>
2025-11-25 08:33:29 +0900

    16017 - Telemarketer or not? 

---
+++ bronze/n16017.py
@@ -0,0 +1,14 @@
+'''
+2025.11.25
+16017 - Telemarketer or not? 
+'''
+
+a = input()
+b = input()
+c = input()
+d = input()
+
+if (b == c) and (a == '8' or a == '9') and (d == '8' or d == '9'):
+    print("ignore")
+else:
+    print("answer")
